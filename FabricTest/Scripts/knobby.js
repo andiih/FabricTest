58f8bfe7d885ad84729bb7f80b9b5b015002f6a0
Andy Hawken <Andy@VaioW8>
2014-02-09 12:54:37 +0000

    Snap magic works

--- FabricTest/Scripts/knobby.js
+++ FabricTest/Scripts/knobby.js
@@ -5,24 +5,25 @@
         this.y = y;
     };
 
+    var calcAngle = function (thisobj, thiscenter) {
+        var thisdX = thisobj.left - thiscenter.x;
+        var thisdY = thisobj.top - thiscenter.y;
+        var thisangle = Math.atan2(thisdY, thisdX);
+        return thisangle;
+    };
+    var calcAnglePoints = function (thisobj, thiscenter) {
+        var thisdX = thisobj.x - thiscenter.x;
+        var thisdY = thisobj.y - thiscenter.y;
+        var thisangle = Math.atan2(thisdY, thisdX);
+        return thisangle;
+    };
+
     var  animateTo=function(pt,obj,center,radius, time) {
-        var dX1 = obj.left - center.x;
-        var dY1 = obj.top - center.y;
-        var angle1 = Math.atan2(dY1, dX1);
         time = time || 1000;
+        var angle1 = calcAngle(obj, center);
+        var angle2 = calcAnglePoints(pt, center);
 
-        var dX2 = pt.x - center.x;
-        var dY2 = pt.y - center.y;
-        var angle2 = Math.atan2(dY2, dX2);
-        var mult = 1;
-        var add = 0;
-
-        if (Math.abs(angle2 - angle1) > Math.PI) {
-            mult = -1;
-            var add = 0;
-            angle1 = angle1 + Math.PI;
-            angle2 = 2 * Math.PI - angle2;
-        }
+        console.log("a1 " + angle1 + " a2 " + angle2);
 
         fabric.util.animate({
             startValue: angle1,
@@ -33,26 +34,19 @@
             easing: function (t, b, c, d) { return c * t / d + b; },
 
             onChange: function (angle) {
-                var newpt = makeRadianCirle(angle*mult+add, radius, center.x, center.y);
+                var newpt = makeRadianCirle(angle, radius, center.x, center.y);
                 obj.set({ top: newpt.y, left: newpt.x }).setCoords();
                 canvas.renderAll();
             }
         });
     };
 
-    var getAngle = function(obj, center) {
-        var dX = obj.left - center.x;
-        var dY = obj.top - center.y;
-        var angle = Math.atan2(dY, dX);
-        return angle;
-
-    };
 
     var constrainToCircle = function (obj, center, radius) {
 //        var dX = obj.left - center.x;
 //        var dY = obj.top - center.y;
         //        var angle = Math.atan2(dY, dX);
-        var angle = getAngle(obj, center);
+        var angle = calcAngle(obj, center);
         var pt = makeRadianCirle(angle, radius, center.x, center.y);
         obj.set({ left: pt.x, top: pt.y }).setCoords();
     };
@@ -73,27 +67,27 @@
     };
 
     var nearestTick = function(angle, ticks) {
-        console.log("near " + angle);
+        //console.log("near " + angle);
         var mindist = 99999;
         var found = 0;
         for (var i = 0; i < ticks.length; i++) {
             var atick = ticks[i];
             var thisdist = Math.abs(angle - atick);
-            console.log("dist " + thisdist + " i " + i);
+            //console.log("dist " + thisdist + " i " + i);
             if (thisdist < mindist) {
                 mindist=thisdist;
                 found = i;
             }
             
         }
-        console.log(found);
+        //console.log(found);
         return ticks[found];
     };
 
 
     var animateToNearest = function (obj, radius, center, time) {
         time = time || 1000;
-        var angle = getAngle(obj, center);
+        var angle = calcAngle(obj, center);
         var nearest = nearestTick(angle, ticks);
         var pt = makeRadianCirle(nearest, radius, center.x, center.y);
         animateTo(pt, obj, center, radius,time);
@@ -107,7 +101,7 @@
     var center = new Point(250, 250);
     var radius = 200;
 
-    var ticks = [0.2, 0.3, 0.4,0.5,1,2,3];
+    var ticks = [0.2, 0.3, 0.4,0.5,-0.1,-0.2,-0.3,-1.9, -3,3];
 
     var track = new fabric.Circle({
         radius: radius, stroke: 'green', fill: 'rgba(0,0,0,0)', strokeWidth: 2, left: center.x, top: center.y, hasBorders: false, hasControls: false, selectable: false, originX: 'center', originY: 'center'
@@ -117,8 +111,8 @@
     var dragMe = new fabric.Circle({ radius: 20, fill: 'green', top: center.y - radius, left: center.x , hasBorders: false, hasControls: false, originX:'center', originY:'center'  });
     dragMe.on('moving', function (options) {
         constrainToCircle(this, center, radius);
-        var angle = getAngle(dragMe, center);
-        console.log(angle);
+        var angle = calcAngle(dragMe, center);
+        //console.log(angle);
     });
     dragMe.on('mouseup', function (options) {
         animateToNearest(dragMe, radius, center,250);
