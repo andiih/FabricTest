4074c9acd37d016ca04f11ba1e5ac48fa0d54478
Andy Hawken <andy@desktop>
2014-02-15 19:24:05 +0000

    Smoother

--- FabricTest/Scripts/knobby.js
+++ FabricTest/Scripts/knobby.js
@@ -25,7 +25,7 @@
         return new Point(cx, cy);
     };
 
-    var  animateTo=function(pt,obj,center,radius, time) {
+    var  animateTo=function(pt,obj,center,radius, time, canvas) {
         time = time || 1000;
         var angle1 = calcAngle(obj, center);
         var angle2 = calcAnglePoints(pt, center);
@@ -82,12 +82,12 @@
     };
 
 
-    var animateToNearest = function (obj, radius, center, time) {
+    var animateToNearest = function (obj, radius, center, time, ticks, canvas) {
         time = time || 1000;
         var angle = calcAngle(obj, center);
         var nearest = nearestTick(angle, ticks);
         var pt = makeRadianCirle(nearest, radius, center);
-        animateTo(pt, obj, center, radius,time);
+        animateTo(pt, obj, center, radius,time, canvas);
 
     };
 
@@ -107,11 +107,7 @@
             var posiitonForChar = makeRadianCirle(angle, radius, center);
             var text = new fabric.Text(c,font);
             var ratio =  text.getBoundingRectWidth() / emwidth;
-
-            //console.log(c + ": " + ratio + " Em: " + text.getBoundingRectWidth()) + "/" + emwidth;
-            
             text.set({ angle: ang, top: posiitonForChar.y, left: posiitonForChar.x, }).setCoords();
-            //canvas.add(text);
             group.push(text);
             ang += inc + fontadjustinc * ratio;
             angle += spacing  + fontadjust* ratio;
@@ -119,74 +115,108 @@
         canvas.add(new fabric.Group(group, { hasBorders: false, hasControls: false, selectable: false, originX: 'center', originY: 'center' }));
     };
 
-    // create a wrapper around native canvas element (with id="c")
-    var canvas = new fabric.Canvas('c');
 
-    var center = new Point(300, 300);
-    var radius = 200;
+    var main = function() {
 
-    var ticks = [0.4, 0.5, 0.6,0.7,-0.1,-0.2,-0.3,-1.9, -3,3];
+        // create a wrapper around native canvas element (with id="c")
+        var canvas = new fabric.Canvas('c');
 
-    var track = new fabric.Circle({
-        radius: radius, stroke: 'green', fill: 'rgba(0,0,0,0)', strokeWidth: 4, left: center.x, top: center.y, hasBorders: false, hasControls: false, selectable: false, originX: 'center', originY: 'center'
-    });
-    canvas.add(track);
+        var center = new Point(300, 300);
+        var radius = 200;
 
-    var maskwidth = 220;
-    var mask = new fabric.Circle({
-        radius: radius+1+maskwidth/2, stroke: '#006699', fill: 'rgba(0,0,0,0)', strokeWidth: maskwidth, left: center.x, top: center.y, hasBorders: false, hasControls: false, selectable: false, originX: 'center', originY: 'center'
-    });
-    canvas.add(mask);
+        var ticks = [0.4, 0.5, 0.6, 0.7, -0.1, -0.2, -0.3, -1.9, -3, 3];
 
-    var dragMe = new fabric.Circle({ radius: 20, fill: 'green', top: center.y - radius, left: center.x , hasBorders: false, hasControls: false, originX:'center', originY:'center'  });
+        var track = new fabric.Circle({
+            radius: radius,
+            stroke: 'green',
+            fill: 'rgba(0,0,0,0)',
+            strokeWidth: 4,
+            left: center.x,
+            top: center.y,
+            hasBorders: false,
+            hasControls: false,
+            selectable: false,
+            originX: 'center',
+            originY: 'center'
+        });
+        canvas.add(track);
 
-    var constrain = false;
+        var maskwidth = 220;
+        var mask = new fabric.Circle({
+            radius: radius + 1 + maskwidth / 2,
+            stroke: '#006699',
+            fill: 'rgba(0,0,0,0)',
+            strokeWidth: maskwidth,
+            left: center.x,
+            top: center.y,
+            hasBorders: false,
+            hasControls: false,
+            selectable: false,
+            originX: 'center',
+            originY: 'center'
+        });
+        canvas.add(mask);
 
-    dragMe.on('moving', function (options) {
-        constrainToCircle(this, center, radius);
-        constrain = true;
-    });
+        var dragMe = new fabric.Circle({ radius: 20, fill: 'green', top: center.y - radius, left: center.x, hasBorders: false, hasControls: false, originX: 'center', originY: 'center' });
+
+        var constrain = false;
+
+        dragMe.on('moving', function(options) {
+            constrainToCircle(this, center, radius);
+            constrain = true;
+        });
+
+        dragMe.on('mouseup', function(options) {
+            animateToNearest(dragMe, radius, center, 250, ticks, canvas);
+            constrain = false;
+        });
+
+
+        for (var i = 0; i < ticks.length; i++) {
+            var tick = ticks[i];
+            drawTick(tick, center, radius + 10, 20, canvas);
+        }
+
+        animateToNearest(dragMe, radius, center, 250, ticks, canvas);
+        canvas.add(dragMe);
+
+        drawOnACurve("Washing Machines", ticks[0], radius + 50, center, 0.04, 0.02, canvas);
+        drawOnACurve("Dishwashers", ticks[6], radius + 50, center, 0.04, 0.02, canvas);
+        drawOnACurve("Regrigerators", ticks[7], radius + 50, center, 0.04, 0.02, canvas);
+        drawOnACurve("Start", ticks[8], radius + 50, center, 0.04, 0.02, canvas);
+
+        $('#behind').click(function() {
+            alert('clicked');
+        });
+
+
+        var snapBack = function(angle) {
+            var time = 250;
+            var nearest = nearestTick(angle, ticks);
+            var pt = makeRadianCirle(nearest, radius, center);
+            animateTo(pt, dragMe, center, radius, time, canvas);
+            var pixels = ((nearest + Math.PI) / conversionFactor) * 8;
+            sk.setScrollTop(pixels);
+            console.log(pixels);
+        };
+
+        var snapBackHandle = null;
+        var listener = function(e) {
+            if (constrain) return;
+
+            $('#anc').text(e.curTop);
+            var angle = (e.curTop / 8) * conversionFactor - Math.PI;
+
+            if (snapBackHandle) clearTimeout(snapBackHandle);
+            snapBackHandle = window.setTimeout(function () { snapBack(angle); }, 250);
+            var pt = makeRadianCirle(angle, radius, center);
+            dragMe.set({ top: pt.y, left: pt.x }).setCoords();
+            canvas.renderAll();
+        };
+
+        sk = skrollr.init({ forceHeight: true, beforerender: listener });
+    }();
+
     
-    dragMe.on('mouseup', function (options) {
-        animateToNearest(dragMe, radius, center, 250);
-        constrain = false;
-    });
-    
-
-    for (var i = 0; i < ticks.length; i++) {
-        var tick = ticks[i];
-        drawTick(tick, center, radius + 10, 20, canvas);
-    }
-
-    animateToNearest(dragMe, radius, center);
-    canvas.add(dragMe);
-
-    //drawOnACurve("Washing Machines", ticks[0], radius + 50, center, 0.04, 0.02, canvas);
-    drawOnACurve("Dishwashers", ticks[6], radius + 50, center, 0.04, 0.02, canvas);
-    //drawOnACurve("Start", ticks[7], radius + 50, center, 0.04, 0.02, canvas);
-    //drawOnACurve("Regrigerators", ticks[9], radius + 50, center, 0.04, 0.02, canvas);
-//    var rad = radius + 50;
-//    var ct = new CurvedText(canvas, { radius: rad, top: center.y, left: center.x + rad, angle: 45, spacing: 3, text: "Washing Machines" });
-//    var ct2 = new CurvedText(canvas, { radius: rad, top: center.y, left: center.x + rad, angle: 45, spacing: 3, text: "Dishwashers" });
-//    var ct3 = new CurvedText(canvas, { radius: rad, top: center.y, left: center.x + rad, angle: 45, spacing: 3, text: "Tumble Dryers" });
-
-    $('#behind').click(function() {
-        alert('clicked');
-    });
-
-    var listener = function (e) {
-        if (constrain) return;
-        $('#anc').text(e.curTop);
-        var angle = (e.curTop/8) * conversionFactor- Math.PI;
-        //var time = 500;
-        //var nearest = nearestTick(angle, ticks);
-        //var pt = makeRadianCirle(nearest, radius, center);        
-        var pt = makeRadianCirle(angle, radius, center);
-        dragMe.set({ top: pt.y, left: pt.x }).setCoords();
-        canvas.renderAll();
-        //animateTo(pt, dragMe, center, radius, time);
-    };
-
-    sk=skrollr.init({ forceHeight: true, beforerender: listener });
 }();
 
